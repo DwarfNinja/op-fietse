baf4dfe0b6203d5d1591e1a8764fbcda58f07edf
DwarfNinja <cenduroyib@gmail.com>
2023-03-30 18:28:57 +0200

    Feat: add header bar
    Header bar displays total estimated repair time

---
+++ components/header-bar.js
@@ -0,0 +1,52 @@
+import { LitElement, html, css } from 'lit';
+import { storageService } from '../services';
+
+export class HeaderBar extends LitElement {
+  static get styles() {
+    return css`
+      #header-bar {
+        display: flex; 
+        justify-content: center; 
+        width: 100%; 
+        position: fixed; 
+        background-color: cornflowerblue; 
+        font-family: sans-serif;
+      }
+    `;
+  }
+
+  static get properties() {
+    return {
+      repairList: [],
+      totalTimeIndication: 0,
+    };
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    this.repairList = storageService.getRepairsFromLocalStorage();
+    let minutes = 0;
+    this.repairList.forEach((repair) => { minutes += parseInt(repair.timeIndication, 10); });
+    this.totalTimeIndication = this.convertMinutesToTime(minutes);
+  }
+
+  convertMinutesToTime(minutes) {
+    const m = minutes % 60;
+    const h = (minutes - m) / 60;
+    const HHMM = `${(h < 10 ? '0' : '') + h.toString()}:${m < 10 ? '0' : ''}${m.toString()}`;
+    return HHMM;
+  }
+
+  render() {
+    return html`
+            <div id="header-bar">
+                <div>
+                    <h3>Totale tijdsindicatie reparaties:</h3>
+                    <h3 class="header-menu-item" style="margin: 2rem auto; padding: 0.5rem; justify-self: center; background-color: white; max-width: 5rem; border-radius: 20px; text-align: center;">${this.totalTimeIndication}</h3>
+                </div>
+            </div>
+    `;
+  }
+}
+
+customElements.define('header-bar', HeaderBar);
