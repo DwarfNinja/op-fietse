a92ecc4a420fdb6cbb7c50d323a8b956c653d962
DwarfNinja <cenduroyib@gmail.com>
2023-04-11 13:20:04 +0200

    Refactor: split repairlist slice into seperate class

---
+++ redux/repairlist_slice.js
@@ -0,0 +1,23 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { storageService } from '../services';
+
+export const repairListSlice = createSlice({
+  name: 'repairList',
+  initialState: {
+    value: storageService.getRepairsFromLocalStorage(),
+  },
+  reducers: {
+    updateRepairList: (state) => {
+      state.value = storageService.getRepairsFromLocalStorage().sort((a, b) => a.id > b.id);
+    },
+    setRepairInRepairList: (state, action) => {
+      storageService.setRepairInLocalStorage(action.payload);
+      state.value = storageService.getRepairsFromLocalStorage().sort((a, b) => a.id > b.id);
+    },
+    changeRepairStatus: (state, action) => {
+      const changedRepair = action.payload;
+      storageService.replaceRepair(changedRepair);
+      state.value = storageService.getRepairsFromLocalStorage().sort((a, b) => a.id > b.id);
+    },
+  },
+});
--- redux/store.js
+++ redux/store.js
@@ -1,26 +1,5 @@
-import { configureStore, createSlice } from '@reduxjs/toolkit';
-import { storageService } from '../services';
-
-const repairListSlice = createSlice({
-  name: 'repairList',
-  initialState: {
-    value: storageService.getRepairsFromLocalStorage(),
-  },
-  reducers: {
-    updateRepairList: (state) => {
-      state.value = storageService.getRepairsFromLocalStorage().sort((a, b) => a.id > b.id);
-    },
-    setRepairInRepairList: (state, action) => {
-      storageService.setRepairInLocalStorage(action.payload);
-      state.value = storageService.getRepairsFromLocalStorage().sort((a, b) => a.id > b.id);
-    },
-    changeRepairStatus: (state, action) => {
-      const changedRepair = action.payload;
-      storageService.replaceRepair(changedRepair);
-      state.value = storageService.getRepairsFromLocalStorage().sort((a, b) => a.id > b.id);
-    },
-  },
-});
+import { configureStore } from '@reduxjs/toolkit';
+import { repairListSlice } from './repairlist_slice';
 
 export const { updateRepairList, changeRepairStatus } = repairListSlice.actions;
 
