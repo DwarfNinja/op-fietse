352632a97096ff75ec2c7a755db0d0a151911d39
DwarfNinja <cenduroyib@gmail.com>
2023-04-11 13:38:52 +0200

    Feat: implement redux store in repair card controller
    Setting a repair card in storage is handled by state action

--- redux/store.js
+++ redux/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { repairListSlice } from './repairlist_slice';
 
-export const { updateRepairList, changeRepairStatus } = repairListSlice.actions;
+export const { updateRepairList, setRepairInRepairList, changeRepairStatus } = repairListSlice.actions;
 
 export const store = configureStore(
   {
